46415b92b6fc4f5db2f95d05ed01562c79c03b02
Florian Loch <florian.loch@gmail.com>
2016-12-05 00:47:21 +0100

    Update image to mjtest, bundle dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM ubuntu:16.04
 MAINTAINER florian.loch@gmail.com
 
-ADD lit lit
 ADD provision.sh provision.sh
+ADD run_tests.sh run_tests.sh
 
 # This does all the dirty work and creates just one image layer
 RUN ./provision.sh
--- provision.sh
+++ provision.sh
@@ -1,27 +1,25 @@
+#! /usr/bin/env bash
+set -e
+
 # Add source for java8
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 
-apt-get update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 
-apt-get install -y --no-install-recommends make ant build-essential git cmake python maven oracle-java8-installer
+apt-get update
 
+apt-get install -y --no-install-recommends make ant build-essential cmake python python3 maven oracle-java8-installer
+apt-get install -y git # adding --no-install-recommends causes ca certs to be not installed, resulting in tls error when accessing github
 
-# Build and install lit
-git clone --depth=1 http://llvm.org/git/llvm.git
-cp -R llvm/utils/lit /usr/local/bin/llvm-lit
-cp lit /usr/local/bin/lit # containted in this repo, forwards to lit utility
+mkdir -p /mj/deps/
+cd /mj/deps/
+git clone https://github.com/MatzeB/libfirm.git
+git clone https://github.com/MatzeB/jFirm.git
 
-# Build and install FileCheck
-cd llvm
-mkdir MY_BUILD/
-cd MY_BUILD && cmake ../
-make FileCheck
-cp bin/FileCheck /usr/local/bin/FileCheck
-cd ../..
-rm -rf llvm
+make -C ./libfirm/ clean
+ant -buildfile ./jFirm/build.xml
 
 # cleanup
 apt-get purge -y cmake
@@ -29,11 +27,3 @@ apt-get autoremove -y
 apt-get clean -y
 rm -rf /var/lib/apt/lists/*
 rm -rf ~/.local/share/Trash/*
-
-# Test (this leads to non-zero exit code and will ruin your current build)
-# echo "JAVA:"
-# java -version
-# echo "FILECHECK:"
-# FileCheck
-# echo "LIT":
-# lit
